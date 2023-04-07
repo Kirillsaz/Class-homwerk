baa340d74844485a4a99cffefb1513262089ae1e
[Kirill] <[kirreg@8888.ae]>
2023-04-07 14:47:18 +0800

    Updated Task_6.py

--- Class 2/Task_6.py
+++ Class 2/Task_6.py
@@ -1,3 +1,8 @@
-my_list = input("Bведите слова через пробел: ").split()
-for a, el in enumerate(my_list, 1):
-    print(f'{a}. {el[:10]}')
+my_list = input("Введите слова через пробел: ").split()
+n = 1
+for elem in my_list:
+    if len(elem) > 10:
+        print(f"{n}. {elem[:10]}")
+    else:
+        print(f"{n}. {elem}")
+        n += 1
