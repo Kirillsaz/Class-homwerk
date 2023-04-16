fd256711c7fb764ff0454a8a4e1036410bdd9b80
[Kirill] <[kirreg@8888.ae]>
2023-04-17 01:22:38 +0800

    updated Task_1.py at directory Class-5

--- Class 5/Task_1.py
+++ Class 5/Task_1.py
@@ -1,4 +1,8 @@
-out_f = open("out_file.txt", "w", encoding='utf-8')
-str_list = ['stroka_1\n', 'stroka_2\n', 'stroka_3\n']
-out_f.writelines(str_list)
-out_f.close()
+filename = input("Введите имя файла: ")
+with open(filename, 'w') as file:
+    while True:
+        line = input("Введите строку данных: ")
+        if line == "q":
+            break
+        file.write(line + "\n")
+print("Данные записаны в файл ", filename)
